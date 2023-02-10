a2fcb1a414b922abfeffccfba5818fdbd55e15cf
hendrixxD <maticsjnr@gmail.com>
2023-02-10 04:29:55 +0100

    new fix

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -9,7 +9,8 @@
 -- Your script can be executed on any database
 
 CREATE TABLE IF NOT EXISTS users (
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-email CHAR(255) NOT NULL UNIQUE,
-name CHAR(255),
-country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US');
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
