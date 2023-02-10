f486e246a163564e77bbf9a9937ca4a2d6386282
hendrixxD <maticsjnr@gmail.com>
2023-02-10 04:56:35 +0100

    meatasl

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -5,6 +5,11 @@
 -- Column names must be: origin and nb_fans
 -- Your script can be executed on any database
 
-SELECT origin, fans nb_fans
-FROM metal_bands
-ORDER BY nb_fans
+WITH band_fans AS (
+    SELECT origin, COUNT(*) as nb_fans
+    FROM metal_bands
+    GROUP BY origin
+)
+SELECT origin, nb_fans
+FROM band_fans
+ORDER BY nb_fans DESC;
