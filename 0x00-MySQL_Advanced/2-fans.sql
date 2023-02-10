240ad3c215c7e5ca4e279c8c92de8f780639473a
hendrixxD <maticsjnr@gmail.com>
2023-02-10 05:23:25 +0100

    ranks

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -5,11 +5,7 @@
 -- Column names must be: origin and nb_fans
 -- Your script can be executed on any database
 
-WITH band_fans AS (
-    SELECT origin, COUNT(*) as nb_fans
-    FROM metal_bands
-    GROUP BY origin
-)
-SELECT origin, nb_fans
-FROM band_fans
+SELECT origin, SUM(fans) AS nb_fans
+FROM metal_bands
+GROUP BY origin
 ORDER BY nb_fans DESC;
---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,11 @@
+-- Write a SQL script that lists all bands with Glam rock as their main style, ranked by their longevity
+
+-- Import this table dump: metal_bands.sql.zip
+-- Column names must be: band_name and lifespan (in years)
+-- You should use attributes formed and split for computing the lifespan
+-- Your script can be executed on any database
+
+SELECT band_name, (formed / split) lifespan
+FROM metal_bands
+WHERE main_style = 'Glam rock'
+ORDER BY lifespanf DESC
