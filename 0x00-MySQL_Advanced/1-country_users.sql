0425150a2d337746f89bbe52fb71974a3eea6ce3
hendrixxD <maticsjnr@gmail.com>
2023-02-10 03:04:27 +0100

    typo fix

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -16,7 +16,7 @@ CREATE TABLE IF NOT EXISTS users (
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 email CHAR(255) NOT NULL UNIQUE,
 name CHAR(255),
-COUNTRY ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US');
+country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US');
 END$$
 -- execute the below to resume the use of ;
 -- DELIMITER;##
