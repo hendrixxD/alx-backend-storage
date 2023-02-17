f2aa0669358c7b72990cb1de660ccef3f49590c9
hendrixxD <maticsjnr@gmail.com>
2023-02-17 22:28:46 +0100

    added decorators

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,11 +2,69 @@
 """
 writing strings to redis
 """
-import functools
+from functools import wraps
 import redis
 from typing import Union, Optional, Callable
 from redis.typing import Union
 import uuid
+
+
+def count_calls(method: Callable) -> Callable:
+    """
+    count function calls
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """
+        wrapper function
+        """
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """
+    function call history
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """
+        wrapper function
+        """
+        input_key = method.__qualname__ + ":inputs"
+        output_key = method.__qualname__ + ":outputs"
+
+        self._redis.rpush(input_key, str(args))
+        output = method(self, *args, **kwargs)
+        self._redis.rpush(output_key, str(output))
+
+        return output
+    return wrapper
+
+
+def replay(method: Callable) -> None:
+    """
+    function to display the history of
+    calls of a particular function
+    """
+    key = method.__qualname__
+    redis_cache = redis.Redis()
+    input_key = key + ":inputs"
+    output_key = key + ":outputs"
+
+    inputs = redis_cache.lrange(input_key, 0, -1)
+    outputs = redis_cache.lrange(output_key, 0, -1)
+
+    calls = redis_cache.get(key).decode('utf-8')
+    print("{} was called {} times:".format(key, calls))
+
+    for i, o in zip(inputs, outputs):
+        i = i.decode('utf-8')
+        o = o.decode('utf-8')
+
+        print("{}(*{}) -> {}".format(key, i, o))
 
 
 class Cache:
@@ -22,15 +80,7 @@ class Cache:
         # flush instance
         self._redis.flushdb
 
-    @staticmethod
-    def count_calls(fn: Callable) -> Callable:
-        @functools.wraps(fn)
-        def wrapped(self, *args, **kwargs):
-            key = fn.__qualname__
-            self._calls.incr(key)
-            return fn(self, *args, **kwargs)
-        return wrapped
-
+    @call_history
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
