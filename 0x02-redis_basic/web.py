7a31a46215d698f142c4fe9bfdc31c95183c2df4
hendrixxD <maticsjnr@gmail.com>
2023-02-18 23:35:06 +0100

    git push

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -3,13 +3,42 @@
 implementing an expiring web cache and tracker
 """
 
+from functools import wraps
 import redis
-import requests
+import requests as req
+from typing import Callable
 
+rds = redis.Redis()
+"""redis client"""
+
+
+def count_requests(method: Callable) -> Callable:
+    """ Decortator for counting how many times a request
+    has been made """
+
+    @wraps(method)
+    def wrapper(url):
+        """ Wrapper for decorator functionality """
+        rds.incr(f"count:{url}")
+        cached_html = rds.get(f"cached:{url}")
+        if cached_html:
+            return cached_html.decode('utf-8')
+
+        html = method(url)
+        rds.setex(f"cached:{url}", 10, html)
+        return html
+
+    return wrapper
+
+
+@count_requests
 def get_page(url: str) -> str:
     """
     args:
         url: str
-    return -> 
+    return ->
     """
 
+    res = req.get(url)
+
+    return res.text
