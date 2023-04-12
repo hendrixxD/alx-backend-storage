0ca6c8fd950e94a529f485a18a395777dedf213b
hendrixxD <maticsjnr@gmail.com>
2023-04-12 17:52:37 +0100

    last files commit

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,22 +1,19 @@
 #!/usr/bin/env python3
-"""
-Task 14. Top students
-"""
+""" Top students """
 
 
 def top_students(mongo_collection):
-    """
-    args:
-        mongo_collection
-
-    return -> list
-    """
-    # Find all students and sort by average score
-    students = mongo_collection.aggregate([
-        {"$project": {"name": 1, "averageScore": {"$avg": "$scores.score"}}},
-        {"$sort": {"averageScore": -1}}
-    ])
-    # Convert the cursor to a list
-    students = list(students)
-    # Return the list of students
-    return students if students else []
+    """returns all students sorted by average score"""
+    top_stds = mongo_collection.aggregate([
+            {
+                "$project":
+                {
+                    "name": "$name",
+                    "averageScore": {"$avg": "$topics.score"}
+                }
+            },
+            {
+                "$sort": {"averageScore": -1}
+            }
+        ])
+    return top_stds
